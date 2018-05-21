b4e3c2c0058e4d72ef554b56caaeb9fb1a92cf27
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2018-05-20 21:42:27 -0300

    Adjustment on scroll position

--- TarcisioCortes/js/main.js
+++ TarcisioCortes/js/main.js
@@ -6,6 +6,7 @@ $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
+    var navbarOffset = 50;
     // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
@@ -20,20 +21,20 @@ $('a[href*="#"]')
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - navbarOffset
         }, 800, function() {
           // Callback after animation
           // Must change focus!
+          // OBS: commented lines below in order to ensure offset is respected
           var $target = $(target);
-          $target.focus();
+//          $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+//            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+//            $target.focus(); // Set focus again
           };
         });
       }
     }
   });
-
